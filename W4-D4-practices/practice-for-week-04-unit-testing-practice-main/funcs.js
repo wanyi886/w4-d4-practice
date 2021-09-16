69a105022f90b7ebcb23f16546b7214094544069
Wanyi <wanyilee0805@gmail.com>
2021-09-16 12:09:17 -0700

    practices

---
+++ W4-D4-practices/practice-for-week-04-unit-testing-practice-main/funcs.js
@@ -0,0 +1,24 @@
+function isFive(num) {
+  // Your code here
+  return num === 5;
+}
+
+function isOdd(number) {
+  // Your code here
+  if (typeof number !== 'number'){
+    throw Error('not a number')
+  }
+  return number % 2 !== 0;
+}
+
+function myRange(min, max, step = 1) {
+  // Your code here
+  let arr = [];
+  for (let i = min; i <= max; i += step){
+    arr.push(i);
+  }
+  return arr;
+}
+
+
+module.exports = { isFive, isOdd, myRange };
---
+++ W4-D4-practices/practice-for-week-04-unit-testing-practice-main/test/class-spec.js
@@ -0,0 +1,49 @@
+const chai = require("chai");
+const { before } = require("mocha");
+const expect = chai.expect;
+
+const { Word } = require("../class");
+
+let newWord;
+beforeEach(() => {
+  newWord = new Word('computer')
+})
+
+describe("Word", function () {
+  describe("Word constructor function", function () {
+    it('should have a "word" property', function () {
+      // expect.fail("replace with your code");
+
+      expect(newWord).to.have.property("word")
+    });
+
+    it('should set the "word" property when a new word is created', function () {
+      // const newWord = new Word("hillo");
+      expect(newWord.word).to.equal("computer")
+
+    });
+  });
+
+  describe("removeVowels function", function () {
+    it("should return a the word with all vowels removed", function () {
+      // expect.fail("replace with your code");
+      // const newWord = new Word("hillo");
+      const result = newWord.removeVowels();
+      expect(result).to.equal('cmptr');
+    });
+  });
+
+  describe("removeConsonants function", function () {
+    it("should return the word with the consonants removed", function () {
+      const newWord = new Word("hillo");
+      const result = newWord.removeConsonants();
+      expect(result).to.equal('io');
+    });
+  });
+
+  describe("pigLatin function", function () {
+    it("should return the word converted to pig latin", function () {
+      expect.fail("replace with your code");
+    });
+  });
+});
